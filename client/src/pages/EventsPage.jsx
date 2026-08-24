cebca36ccdfa858f7d432b5121c211f35e3bf8d8
swapnilbhadra2004 <bhadraswapnil@gmail.com>
2026-08-24 21:04:58 +0530

    missing pages. placeholders placed

---
+++ client/src/pages/EventsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Login Page</h2>
+      <p>Login form will go here</p>
+    </div>
+  );
+}
+
+export default LoginPage;
